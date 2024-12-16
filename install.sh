b807767aba5f41de60ad9861da8135f38e918c0f
yayalingo <74298875+yayalingo@users.noreply.github.com>
2024-12-16 16:55:12 +0800

    Create install.sh

---
+++ install.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+
+# Install Go
+echo "Installing Go..."
+wget https://go.dev/dl/go1.20.7.linux-amd64.tar.gz -O /tmp/go1.20.7.linux-amd64.tar.gz
+sudo tar -C /usr/local -xvzf /tmp/go1.20.7.linux-amd64.tar.gz
+rm /tmp/go1.20.7.linux-amd64.tar.gz
+
+# Add Go binary path to .bashrc
+echo "Adding Go to PATH..."
+echo "export PATH=\$PATH:/usr/local/go/bin" >> ~/.bashrc
+source ~/.bashrc
+
+# Verify Go installation
+echo "Go installation completed. Verifying..."
+go version
+
+# Install Subfinder
+echo "Installing Subfinder..."
+go install -v github.com/projectdiscovery/subfinder/v2/cmd/subfinder@latest
+
+# Install Httprobe
+echo "Installing Httprobe..."
+go install github.com/tomnomnom/httprobe@latest
+
+# Install EyeWitness
+echo "Cloning and setting up EyeWitness..."
+git clone https://github.com/ChrisTruncer/EyeWitness.git
+cd EyeWitness || exit
+cd ..
+echo "Installation is complete."
